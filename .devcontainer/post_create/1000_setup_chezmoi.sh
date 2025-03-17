728a014786c89e48dcb038bb07078cdb7b6d8bd4
Di-Is <hommageMe@gmail.com>
2025-03-17 12:57:13 +0000

    refactor: move chezmoi init

--- .devcontainer/post_create/1000_setup_chezmoi.sh
+++ .devcontainer/post_create/1000_setup_chezmoi.sh
@@ -6,8 +6,7 @@ sudo chown `id -u`:`id -g` -R /home/ubuntu/.local
 
 if ! command -v chezmoi >/dev/null 2>&1; then
     sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+    chezmoi init --apply ${GITHUB_USERNAME}
 else
     chezmoi update
 fi
-
-chezmoi init --apply ${GITHUB_USERNAME}
