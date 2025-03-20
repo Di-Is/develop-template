ac0aa962d426b5de1f834f4306b18233a5d1b8a6
Di-Is <hommageMe@gmail.com>
2025-03-20 23:08:33 +0900

    change: add docker compose project name to username

--- .devcontainer/initialize/001_generate_env.sh
+++ .devcontainer/initialize/001_generate_env.sh
@@ -48,7 +48,7 @@ else
   if command -v git >/dev/null 2>&1; then
     repo_path=$(git rev-parse --show-toplevel 2>/dev/null || true)
     if [ -n "$repo_path" ]; then
-      COMPOSE_PROJECT_NAME=$(basename "$repo_path")
+      COMPOSE_PROJECT_NAME=$(basename "$repo_path")-$(id -un)
     else
       echo "Error: Not inside a git repository. Cannot determine COMPOSE_PROJECT_NAME."
       exit 1
