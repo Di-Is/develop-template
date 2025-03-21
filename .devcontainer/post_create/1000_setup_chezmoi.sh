5d1a83ab94e76b9dce897c894f2cfaa6e023215c
Di-Is <hommageMe@gmail.com>
2025-03-21 10:54:26 +0000

    add: chezmoi cache reset

--- .devcontainer/post_create/1000_setup_chezmoi.sh
+++ .devcontainer/post_create/1000_setup_chezmoi.sh
@@ -6,6 +6,9 @@ sudo mkdir -p /home/ubuntu/.config
 sudo chown $(id -u):$(id -g) -R /home/ubuntu/.local
 sudo chown $(id -u):$(id -g) -R /home/ubuntu/.config
 
+# reset cache
+chezmoi state reset --force
+
 if ! command -v chezmoi >/dev/null 2>&1; then
     sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
     chezmoi init --apply ${GITHUB_USERNAME}
