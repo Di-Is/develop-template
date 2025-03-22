a1e7a9e11aa6f6c361e014cba717910ca84712a0
Di-Is <hommageMe@gmail.com>
2025-03-22 08:17:56 +0000

    change: setup chezmoi pass to arg

--- .devcontainer/post_create/1000_setup_chezmoi.sh
+++ .devcontainer/post_create/1000_setup_chezmoi.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# escape volume mount error
+# Check that exactly one argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <GITHUB_USERNAME>" >&2
+    exit 1
+fi
+
+GITHUB_USERNAME=$1
+
+# Create necessary directories and adjust permissions to avoid volume mount errors
 sudo mkdir -p /home/ubuntu/.local
 sudo mkdir -p /home/ubuntu/.config
 sudo chown $(id -u):$(id -g) -R /home/ubuntu/.local
 sudo chown $(id -u):$(id -g) -R /home/ubuntu/.config
 
-# reset cache
+# Reset cache
 chezmoi state reset --force
 
 if ! command -v chezmoi >/dev/null 2>&1; then
+    # Install chezmoi if not available and initialize with the provided GitHub username
     sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
-    chezmoi init --apply ${GITHUB_USERNAME}
+    chezmoi init --apply "${GITHUB_USERNAME}"
 else
+    # Update chezmoi if it is already installed
     chezmoi update
 fi
